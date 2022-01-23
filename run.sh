fb90dcef6f29fb6192df35978c638b27778f8792
Ilya Kreymer <ikreymer@gmail.com>
2022-01-23 02:16:47 +0000

    run.sh: undo change

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # can configure token here
 #export AUTH_TOKEN=token
-uvicorn authsign.main:app --port 8082 --host 0.0.0.0 --log-config log.json
+uvicorn authsign.main:app --port 8080 --host 0.0.0.0 --log-config log.json
