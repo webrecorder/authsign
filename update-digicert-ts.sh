8374943ec391e14223860d82da83285b3dbf0d1b
Ilya Kreymer <ikreymer@gmail.com>
2026-08-20 15:42:19 -0700

    update to latest digicert cert update ts-digicert.pem chain, ensure tests pass

--- update-digicert-ts.sh
+++ update-digicert-ts.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 # Update cert and root from https://knowledge.digicert.com/general-information/rfc3161-compliant-time-stamp-authority-server
 
-curl "https://knowledge.digicert.com/content/dam/kb/attachments/time-stamp/TSACertificate.cer" > ./authsign/trusted/ts-digicert.pem
+# old:
+#curl -o ./leaf.pem "https://knowledge.digicert.com/content/dam/kb/attachments/time-stamp/TSACertificate.cer"
+
+
+# Get Lead and convert to PEM
+curl -o ./leaf.cer "https://knowledge.digicert.com/content/dam/kb/attachments/time-stamp/DigiCertSHA256RSA4096TimestampResponder20251.cer"
+
+openssl x509 -inform DER -in ./leaf.cer -out ./leaf.pem -outform PEM
+
+# Get Intermediate
+curl -o ./intermediate.pem "https://knowledge.digicert.com/content/dam/kb/attachments/time-stamp/DigiCertTrustedG4TimeStampingRSA4096SHA2562025CA1.pem"
+
+# Get Root
+# Note: this is actual a PEM not a CER
+curl -o ./root.pem "https://knowledge.digicert.com/content/dam/kb/attachments/time-stamp/DigiCertTrustedRootG4.cer"
+
+
+# concat all
+cat ./leaf.pem ./intermediate.pem ./root.pem > ./authsign/trusted/ts-digicert.pem
+
