b8af6dd34cccae7662b89f9738ab4681bc270f75
Ilya Kreymer <ikreymer@gmail.com>
2026-08-20 10:10:37 -0700

    reformat

--- authsign/patch_rfc3161ng.py
+++ authsign/patch_rfc3161ng.py
@@ -80,6 +80,6 @@ def _patched_check_timestamp(
 
 
 def apply_patch():
-    """ apply monkey-patch to rfc3161ng """
+    """apply monkey-patch to rfc3161ng"""
     rfc3161ng.check_timestamp = _patched_check_timestamp
     rfc3161ng.api.check_timestamp = _patched_check_timestamp
